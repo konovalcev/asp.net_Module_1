1e2c00a0d00d9a3d06f1777e6b5bb9a547ddc22e
aleks <akonovalcev@gmail.com>
2018-11-21 17:39:01 +0300

    Started Lesson5

--- WebStore.Domain/Entities/Base/BaseEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace WebStore.Domain.Entities.Base
-{
-    public class BaseEntity : IBaseEntity
-    {
-        public int Id { get; set; }
-    }
-}
--- WebStore.DomainNew/Entities/Brand.cs
+++ WebStore.DomainNew/Entities/Brand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using WebStore.Domain.Entities.Base;
 
 namespace WebStore.Domain.Entities
@@ -6,8 +7,10 @@ namespace WebStore.Domain.Entities
     /// <summary>
     /// Сущность бренд
     /// </summary>
+    [Table("Brands")]
     public class Brand: NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
     }
 }
